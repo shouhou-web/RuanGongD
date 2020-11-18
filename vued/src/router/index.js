920102759504db5e88249ac4d7d8bbf4e4f7c0b1
Codevka <1724707236@qq.com>
2020-11-18 23:29:12 +0800

    add UserPosts

--- vued/src/router/index.js
+++ vued/src/router/index.js
@@ -8,6 +8,7 @@ const Test = () => import("views/Test.vue");
 // 论坛相关组件
 const ForumHome = () => import("views/Forum/ForumHome.vue");
 const ForumSector = () => import("views/Forum/ForumSector.vue");
+const UserPosts = () => import("views/Forum/UserPosts.vue");
 
 // 首页相关组件
 const Home = () => import("views/Home/Home.vue");
