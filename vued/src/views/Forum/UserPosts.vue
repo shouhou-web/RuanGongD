ea9f211f7a980419c3d83ee1564bdf34b72823c7
Codevka <1724707236@qq.com>
2020-11-18 23:27:44 +0800

    add UserPosts

---
+++ vued/src/views/Forum/UserPosts.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- 一个用户的所有动态 -->
+  <div id="UserPosts">
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserPosts",
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped></style>
