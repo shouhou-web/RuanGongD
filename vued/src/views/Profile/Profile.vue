7dab1422970ac156f401579a130f3f8e62ecc470
samshui <18373612@buaa.edu.cn>
2020-11-20 19:05:14 +0800

    初版个人信息页

--- vued/src/views/Profile/Profile.vue
+++ vued/src/views/Profile/Profile.vue
@@ -4,19 +4,38 @@
     <div id="profile-firstlayer" class="firstlayer-outterstyle">
       <div id="headshot" class="headshot-style">
         <div id="img-statetag" class="imgtag-style">
-          <div id="img" class="img-style" @click="changeheadshot()"></div>
-          <div id="userstate" class="userstate-style" @click="alertIdent()">未认证用户</div>
+          <div id="img" class="img-style"></div>
         </div>
         <div id="simpleinfo" class="simpleinfo-style">
-          <div id="username" class="username-style">Yu Haomiao</div>
-          <div id="userinfo" class="userinfo-style">This is Samshui's intro,welcome to my homepage and wish you can use this web softly.</div>
+          <div id="username" class="username-style">
+            {{ userName }}
+          </div>
+          <div class="info-tags">
+            <div id="userDegree" class="userdegree-style">
+              {{ retUserDegree() }}
+            </div>
+            <div id="userDegree" class="userdegree-style">
+              {{ retUserState() }}
+            </div>
+          </div>
+
         </div>
       </div>
       <div id="publish" class="publish-style">
-        <div id="publish-button" class="publishbutton-style" @click="alertPublish()">发布</div>
+        <div id="publish-button" class="publishbutton-style">发布</div>
       </div>
     </div>
     <div id="profile-secondlayer" class="secondlayer-outterstyle">
+      <div class="choose-style">
+        <div class="secondlayer-tagstyle" @click="changeChosenModle(0)">收藏</div>
+        <div class="whiteblock"></div>
+        <div class="secondlayer-tagstyle" @click="changeChosenModle(1)">关注</div>
+      </div>
+      <div class="chooseSlice-style">
+        <div :class="{ 'sliceRight': this.chosenModle == true, 'sliceLeft': this.chosenModle == false}"></div>
+      </div>
+    </div>
+    <div id="profile-fourthlayer" class="fourthlayer-outterstyle">
       <div id="leftpart" class="leftpart-style"></div>
       <div id="rightpart" class="rightpart-style"></div>
     </div>
@@ -27,17 +46,32 @@
 export default {
   name: "Profile",
   data() {
-    return {};
+    return {
+      userName: 'Yu Haomiao',
+      userState: 1,
+      userDegree: 1,
+      chosenModle: false
+    };
   },
   methods: {
-    changeheadshot() {
-      this.$message.success("点击头像");
+    retUserState() {
+      if (this.userState == 0) return "管理员";
+      else if (this.userState == 1) return "已认证用户";
+      else if (this.userState == 2) return "未认证用户";
+      else return "";
     },
-    alertIdent() {
-      this.$message.info("您还未认证");
+    retUserDegree() {
+      if (this.userDegree == 0) return "高中";
+      else if (this.userDegree == 1) return "本科生";
+      else if (this.userDegree == 2) return "研究生";
+      else if (this.userDegree == 3) return "博士生";
+      else if (this.userDegree == 4) return "博士后";
+      else return "";
     },
-    alertPublish() {
-      this.$message.error("您还未认证，无法发表内容");
+    changeChosenModle(nowModle) {
+      if (this.chosenModle != nowModle)
+        this.chosenModle = !this.chosenModle;
+      console.log("切换模式" + this.chosenModle);
     }
   },
   components: {},
@@ -46,37 +80,30 @@ export default {
 
 <style scoped>
 .firstlayer-outterstyle {
-  margin-left: 5vw;
-  margin-top: 10px;
   background-color: white;
-  border-radius: 5px;
-  width: 90vw;
+  width: 100vw;
   height: 150px;
   display: flex;
   flex-direction: row;
 }
 
-.secondlayer-outterstyle {
-  margin-left: 5vw;
-  margin-top: 10px;
-  background-color: rgba(255, 255, 255, 0.336);
-  border-radius: 5px;
-  width: 90vw;
-  height: 450px;
+.fourthlayer-outterstyle {
+  width: 100vw;
+  height: auto;
   display: flex;
   flex-direction: row;
 }
 
 .leftpart-style {
   margin: 2%;
-  width: 46%;
+  width: 66%;
   height: 300px;
   background-color: white;
 }
 
 .rightpart-style {
   margin: 2%;
-  width: 46%;
+  width: 26%;
   height: 200px;
   background-color: white;
 }
@@ -95,13 +122,15 @@ export default {
   min-height: 80px;
   width: 80px;
   height: 80px;
-  border-radius: 50%;
+  border-radius: 5%;
   /* background-color: rgb(138, 157, 160); */
   background-image: url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605627415356&di=42ef36c7ce934ca22a1cbc05fac29c5d&imgtype=0&src=http%3A%2F%2Fww1.sinaimg.cn%2Fmw690%2F5301ff11ly1gb58jwjhikj20p00p0q4l.jpg);
   background-size: 80px 80px;
+  cursor: pointer;
 }
 
 .simpleinfo-style {
+  margin-top: 10px;
   width: calc(90% - 80px);
   height: auto;
 }
@@ -122,27 +151,38 @@ export default {
   width: auto;
   height: auto;
   min-width: 80px;
-  border-radius: 5px;
   text-align: center;
   background-color: rgba(223, 203, 137, 0.411);
   color: rgb(241, 125, 16);
   font-size: 1.5vh;
+  cursor: pointer;
 }
 
 .username-style {
-  border-radius: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  font-size: 2vw;
+  /*background-color: rgba(233, 233, 233, 0.651);*/
+  margin-left: 10px;
+  width: auto;
+  max-height: 50px;
+  color: #8eacc5;
+}
+
+.userdegree-style {
   padding: 5px;
   background-color: rgba(233, 233, 233, 0.651);
-  width: 90%;
+  text-align: center;
   margin: 5%;
-  font-size: 2vh;
+  font-size: 1vh;
   font-family: Console;
-  overflow: auto;
-  max-height: 40px;
+  /*min-height: 40px;*/
+  max-width: 90px;
+  min-width: 50px;
+  max-height: 50px;
 }
 
 .userinfo-style {
-  border-radius: 5px;
   padding: 5px;
   background-color: rgba(233, 233, 233, 0.651);
   width: 90%;
@@ -157,11 +197,72 @@ export default {
 
 .publishbutton-style {
   text-align: center;
-  border-radius: 5px;
   background-color: rgba(168, 221, 241, 0.548);
   width: 120px;
   padding: 3%;
   margin-left: calc(50% - 60px);
   margin-top: 60px;
+  cursor: pointer;
+}
+
+.secondlayer-outterstyle {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 40px;
+  /*border: 1px solid red;*/
+  background-color: white;
+  justify-content: center;
+}
+
+.secondlayer-tagstyle {
+  text-align: center;
+  margin: auto 0;
+  /*border: 1px solid black;*/
+  height: auto;
+  width: auto;
+  font-size: larger;
+  cursor: pointer;
+}
+
+.whiteblock {
+  width: 20%;
+  height: 100%;
+}
+
+.choose-style {
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  height: 80%;
+  justify-content: center;
+}
+
+.chooseSlice-style {
+  margin-top: 10px;
+  width: 100vw;
+  height: 10%;
+  /*border: 1px solid black;*/
+}
+
+.sliceLeft {
+  width: 5%;
+  height: 100%;
+  background-color: #8eacc5;
+  margin-left: calc(38% - 20px);
+  transition: all 300ms linear;
+}
+
+.sliceRight {
+  width: 5%;
+  height: 100%;
+  margin-left: calc(58% + 10px);
+  background-color: #96bf47;
+  transition: all 300ms linear;
+}
+
+.info-tags {
+  display: flex;
+  flex-direction: row;
 }
 </style>
