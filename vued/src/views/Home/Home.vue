f489c393149465d5d02b68463011d401fea8141f
shouhou-web <18373337@buaa.edu.cn>
2020-12-15 11:24:11 +0800

    home

--- vued/src/views/Home/Home.vue
+++ vued/src/views/Home/Home.vue
@@ -6,11 +6,40 @@
     <div class="logo">
       <img src="@/assets/image/home/logo.png" alt="" />
     </div>
-    <search-advance @change-list="changeList" @change-time="changeTime" :isShow="true"/>
+    <search-advance
+      @change-list="changeList"
+      @change-time="changeTime"
+      :isShow="true"
+    />
     <div class="footer">
-      <div class="item1"></div>
-      <div class="item2"></div>
-      <div class="item3"></div>
+      <!-- 高被引论文 -->
+      <div class="ciation">
+        <div class="ciation__title">
+          高被引论文
+        </div>
+        <div class="ciation__content">
+          <ul>
+            <li v-for="(item, index) in ciationList" :key="index">
+              <div class="ciation__content__id">
+                <i class="icon--one">{{ index + 1 }}</i>
+              </div>
+              <div class="ciation__content__main">
+                <div class="ciation__content__main__title">
+                  {{ item.title }}
+                </div>
+                <div class="ciation__content__main__detail">
+                  {{ item.author }}; {{ item.venue }}; {{ item.year }}
+                </div>
+              </div>
+              <div class="ciation__content__num">
+                {{ item.ciation }}
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- 收藏论文 -->
+      <div class="likes"></div>
     </div>
   </div>
 </template>
@@ -21,7 +50,15 @@ export default {
   name: "Home",
   data() {
     return {
-      searchList: [],
+      ciationList: [
+        {
+          id: "123",
+          title: "论中国民族民间舞的风格与规格",
+          author: "国华",
+          venue: "人民音乐",
+          year: "1984"
+        }
+      ],
       start: "",
       end: ""
     };
@@ -64,11 +101,56 @@ export default {
   margin: 40px auto;
 }
 
-.item1,
-.item2,
-.item3 {
-  background-color: #eee;
-  width: 300px;
-  height: 350px;
+.ciation {
+  width: 850px;
+}
+
+.ciation__title {
+  font-size: 22px;
+  line-height: 24px;
+}
+
+.ciation__content {
+  height: 320px;
+  margin-top: 30px;
+}
+
+.ciation__content__id {
+  height: 25px;
+  line-height: 25px;
+  width: 25px;
+}
+
+i {
+  display: inline-block;
+  height: 50px;
+  width: 50px;
+  vertical-align: middle;
+  text-align: center;
+  line-height: 50px;
+  font-size: 32px;
+  font-style: normal;
+  transform: scale(0.5);
+  transform-origin: left top;
+}
+
+.icon--one {
+  background: url(https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/cache/scholar_mid/static/protocol/https/xueshu/img/css_sprites_5b400ea.png) -10px -270px;
+  color: #fff;
+}
+
+.icon--two {
+  background: url(https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/cache/scholar_mid/static/protocol/https/xueshu/img/css_sprites_5b400ea.png) -274px -150px;
+  color: #fff;
+}
+
+.icon--three {
+  background: url(https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/cache/scholar_mid/static/protocol/https/xueshu/img/css_sprites_5b400ea.png) -274px -80px;
+  color: #fff;
+}
+
+.icon--other {
+  background: url(https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/cache/scholar_mid/static/protocol/https/xueshu/img/css_sprites_5b400ea.png) -274px -10px;
+  color: #999;
 }
 </style>
