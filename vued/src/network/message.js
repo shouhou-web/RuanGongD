8da211e16d80f6932d44576a0e8268d225763e3f
shouhou-web <18373337@buaa.edu.cn>
2020-12-15 20:46:52 +0800

    message

---
+++ vued/src/network/message.js
@@ -0,0 +1,35 @@
+import { request } from "@/network/request";
+let baseURL = "http://185.133.193.251:8084";
+
+// 获取评论信息
+export function getCommentMsg(userID) {
+  return request(baseURL, {
+    url: "/getCommentMsg",
+    params: {
+      userID
+    },
+    method: "post"
+  });
+}
+
+// 获取系统消息
+export function getSystemMsg(userID) {
+  return request(baseURL, {
+    url: "/getSystemMsg",
+    params: {
+      userID
+    },
+    method: "post"
+  });
+}
+
+// 查看咨询消息
+export function getConsultMsg(userID) {
+  return request(baseURL, {
+    url: "/getConsultMsg",
+    params: {
+      userID
+    },
+    method: "post"
+  });
+}
