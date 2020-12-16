571531fd60a7ac88c9c33cd35b9c1cd38eecb020
Codevka <1724707236@qq.com>
2020-12-16 14:49:36 +0800

    查看用户动态子组件

--- vued/src/network/forum.js
+++ vued/src/network/forum.js
@@ -26,7 +26,7 @@ export function getAllSectors() {
   return request(baseURL, {
     url: "/getAllSectors",
     params: {},
-    method: "post"
+    method: "post",
   });
 }
 
@@ -41,7 +41,7 @@ export function getAllTags() {
   return request(baseURL, {
     url: "/getAllTags",
     params: {},
-    method: "post"
+    method: "post",
   });
 }
 
@@ -63,9 +63,9 @@ export function createPost(createPostForm) {
   return request(baseURL, {
     url: "/createPost",
     params: {
-      createPostForm
+      createPostForm,
     },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -99,9 +99,9 @@ export function getPostInfo(userId, postId) {
     url: "/getPostInfo",
     params: {
       userId,
-      postId
+      postId,
     },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -132,7 +132,7 @@ export function getPosts(sectorId, start, num, sort, keyword) {
   return request(baseURL, {
     url: "/getPosts",
     params: { sectorId, start, num, sort, keyword },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -148,7 +148,7 @@ export function isFollowed(userId, sectorId) {
   return request(baseURL, {
     url: "/isFollowed",
     params: { userId, sectorId },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -165,7 +165,7 @@ export function followSector(userId, sectorId) {
   return request(baseURL, {
     url: "/followSector",
     params: { userId, sectorId },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -182,7 +182,7 @@ export function reportPost(userId, postId, reportContent) {
   return request(baseURL, {
     url: "/reportPost",
     params: { userId, postId, reportContent },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -198,7 +198,7 @@ export function deletePost(userId, postId) {
   return request(baseURL, {
     url: "/deletePost",
     params: { userId, postId },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -215,7 +215,7 @@ export function commentPost(userId, postId, commentContent) {
   return request(baseURL, {
     url: "/commentPost",
     params: { userId, postId, commentContent },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -232,7 +232,7 @@ export function reportComment(userId, reportCommentId, reportContent) {
   return request(baseURL, {
     url: "/reportComment",
     params: { userId, reportCommentId, reportContent },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -248,7 +248,7 @@ export function deleteComment(userId, deleteCommentId) {
   return request(baseURL, {
     url: "/deleteComment",
     params: { userId, deleteCommentId },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -265,7 +265,7 @@ export function createConsultation(senderId, receiverId, text) {
   return request(baseURL, {
     url: "/createConsultation",
     params: { senderId, receiverId, text },
-    method: "post"
+    method: "post",
   });
 }
 
@@ -280,6 +280,28 @@ export function getPostNum(sectorId) {
   return request(baseURL, {
     url: "/getPostNum",
     params: { sectorId },
-    method: "post"
+    method: "post",
+  });
+}
+
+/**
+ * 获取一个用户的全部动态
+ * @param {userId}
+ *  userId: 用户Id
+ * @return {posts[{postId, postName, replyNum, viewNum, sectorName, createTime, tags[]}]}
+ *  posts[]是数组,里面的元素是对象:
+ *    postId: 动态id
+ *    postName: 动态名称(标题)
+ *    replyNum: 动态的评论数
+ *    viewNum: 动态的被查看次数
+ *    sectorName: 动态所在分区名称
+ *    createTime: 动态的创建时间
+ *    tags[]: 数组, 动态的所有标签,
+ */
+export function getUserPosts(userId) {
+  return request(baseURL, {
+    url: "/getUserPosts",
+    params: { userId },
+    method: "post",
   });
 }
