6a6b7ab295977d05f9b232079130b9a3f038d728
interlink3290 <lvhx3290@163.com>
2020-12-17 10:33:45 +0800

    管理文献模板完成

--- vued/src/network/literature.js
+++ vued/src/network/literature.js
@@ -1,8 +1,35 @@
 import { request } from "@/network/request";
+let baseURL = "http://185.133.193.251:8081";
 
+// 获取个人文献列表
+export function getMyLiterature(userID) {
+  return request(baseURL, {
+    url: "/getMyLiterature",
+    params: {
+      userID
+    },
+    method: "post"
+  });
+}
+
+// 修改文献链接
+// 获取个人文献列表
+export function editLiterature(userID, Lid, url) {
+  return request(baseURL, {
+    url: "/editLiterature",
+    params: {
+      userID,
+      Lid,
+      url
+    },
+    method: "post"
+  });
+}
+
+//发布文献
 export function createLiterature(createLiForm) {
   //console.log(createLiForm);
-  return request({
+  return request(baseURL, {
     url: "/createLiterature",
     params: {
       createLiForm
